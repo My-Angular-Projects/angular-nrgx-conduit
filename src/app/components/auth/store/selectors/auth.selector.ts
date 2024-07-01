af3d4303b56d80d817324043d8e445b94400bf7d
Valery Semenenko <gearmobile@gmail.com>
2024-07-01 16:43:08 +0300

    refactor: add comment for auth selector

--- src/app/components/auth/store/selectors/auth.selector.ts
+++ src/app/components/auth/store/selectors/auth.selector.ts
@@ -1,6 +1,11 @@
+/** Не стал менять на FeatureCreator - просто чтобы показать, что могу и вручную делать селекторы */
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { authFeatureKey } from '../reducers/auth.reducer';
-import { IAuthState } from '../../../../interfaces';
+import {
+  IAuthState,
+  IBackendErrors,
+  IRegisteredUser,
+} from '../../../../interfaces';
 
 // Deprecate the RootState generic of createFeatureSelector
 // https://github.com/ngrx/platform/issues/3179
@@ -9,25 +14,25 @@ export const authFeatureSelect =
 
 export const isSubmittingSelector = createSelector(
   authFeatureSelect,
-  (state: IAuthState) => state.isSubmitting,
+  (state: IAuthState): boolean => state.isSubmitting,
 );
 
 export const currentUserSelector = createSelector(
   authFeatureSelect,
-  (state: IAuthState) => state.user,
+  (state: IAuthState): IRegisteredUser | null => state.user,
 );
 
 export const isLoggedInSelector = createSelector(
   authFeatureSelect,
-  (state: IAuthState) => state.isLoggingIn,
+  (state: IAuthState): boolean | null => state.isLoggingIn,
 );
 
 export const isAnonymousSelector = createSelector(
   authFeatureSelect,
-  (state: IAuthState) => state.isLoggingIn === false,
+  (state: IAuthState): boolean => state.isLoggingIn === false,
 );
 
 export const authErrorsSelector = createSelector(
   authFeatureSelect,
-  (state: IAuthState) => state.errors,
+  (state: IAuthState): IBackendErrors | null => state.errors,
 );
